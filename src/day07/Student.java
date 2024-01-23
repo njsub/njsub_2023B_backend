cce533c48f83e61c9036e4052b3f128ca47e49b3
njsub <njsub@naver.com>
2024-01-23 18:10:24 +0900

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: 노진섭 <njsub@naver.com>

--- src/day07/Student.java
+++ src/day07/Student.java
@@ -1,11 +1,10 @@
 package day07;
 
+import java.util.Scanner;
+
 public class Student { //cs 클래스 선언
 
     public static void main(String[] args) {//ms 메인선언
-
-
-
 
     }//ce
 }//me
--- src/day08/Korean.java
+++ src/day08/Korean.java
@@ -13,7 +13,10 @@ public class Korean {
         // 객체들간의 다른 값을 가져야 할때
         // 1. 생성자명 작성한다. 단 클래스명도 동일
         // 2. () 안에 매개변수를 정의한다.
+
+
     Korean (String n , String sl){}
+    Korean (){}
     //3. 메소드
 
 
